c10824e8c32f6b4ac21418c3b98fd24c656badb1
Jacob Case <jcase.bus@gmail.com>
2022-01-15 20:23:35 -0700

    project1 with challenges

---
+++ Project1_StormViewer/Project1_StormViewer/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  Project1_StormViewer
+//
+//  Created by Jacob Case on 1/15/22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        
+        // let fm = file manager default from system
+        let fm = FileManager.default
+        
+        // A representation of the code and resources stored in a bundle directory on disk
+        // path = that bundle
+        let path = Bundle.main.resourcePath!
+        
+        // items array is constant collection of items found within file directory
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                // this is a picture to load
+                pictures.append(item)
+            }
+        }
+        pictures = pictures.sorted()
+        print(pictures)
+        
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        cell.textLabel?.font = UIFont(name: "Noteworthy", size: 20)
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            vc.pictureNumber = indexPath.row
+            vc.numPictures = pictures.count
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+}
+
