1a031b7a50d63a1d0cfb5cdad249d9b4f035f2ef
Jacob Case <jcase.bus@gmail.com>
2022-01-26 11:49:13 -0800

    includes challenges 1&2 for project7

--- Project7_WhitehousePetitions/Project7_WhitehousePetitions/ViewController.swift
+++ Project7_WhitehousePetitions/Project7_WhitehousePetitions/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 class ViewController: UITableViewController {
     
     var petitions = [Petition]()
+    
+    // part of challenge 2, duplicate copy of petitions for filtered results only
+    var filteredPetitions = [Petition]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
+        // Challenge 1: Add a Credits button to the top-right corner using UIBarButtonItem. When this is tapped, show an alert telling users the data comes from the We The People API of the Whitehouse.
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(showCredits))
+        
+        // Challenge 2:Let users filter the petitions they see. This involves creating a second array of filtered items that contains only petitions matching a string the user entered. Use a UIAlertController with a text field to let them enter that string.
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(promptSearch))
         
         let urlString: String
 
@@ -32,7 +43,38 @@ class ViewController: UITableViewController {
         } else {
             showError()
         }
+    }
+    // part of challenge 2, method for prompting user to enter a string to search on
+    @objc func promptSearch() {
+        let ac = UIAlertController(title: "Enter Search String", message: nil, preferredStyle: .alert)
+        ac.addTextField()
         
+        let submitString = UIAlertAction(title: "Search", style: .default) { [weak self, weak ac] _ in
+            guard let searchString = ac?.textFields?[0].text else {return}
+            self?.beginSearch(searchString)
+        }
+        
+        ac.addAction(submitString)
+        present(ac, animated: true)
+    }
+    
+    @objc func showCredits() {
+        let ac = UIAlertController(title: "Data comes from:", message: "We The People API", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(ac, animated: true)
+        
+    }
+    
+    // Part of challenge 2; function that backs promptSearch method. Takes user input and extracts relevant petitions
+    // does depend on casing for str
+    func beginSearch(_ str: String) {
+        filteredPetitions.removeAll(keepingCapacity: true)
+        for singlePetition in petitions {
+            if singlePetition.title.contains(str) {
+                filteredPetitions.append(singlePetition)
+            }
+        }
+        tableView.reloadData()
     }
     
     func showError() {
@@ -46,17 +88,21 @@ class ViewController: UITableViewController {
 
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
+            filteredPetitions = petitions
             tableView.reloadData()
         }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        
+        // part of challenge 2 to update tableView to only include filtered results
+        return filteredPetitions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let petition = petitions[indexPath.row]
+        // update for challenge 2, only include filtered results
+        let petition = filteredPetitions[indexPath.row]
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
@@ -64,7 +110,9 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = DetailViewController()
-        vc.detailItem = petitions[indexPath.row]
+        
+        //updated for challenge 2
+        vc.detailItem = filteredPetitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
 
