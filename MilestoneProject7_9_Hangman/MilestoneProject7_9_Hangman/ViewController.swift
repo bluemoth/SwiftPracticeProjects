59887748a8845485a19e28caacaea5a9ef8282cf
Jacob Case <jcase.bus@gmail.com>
2022-02-05 12:09:12 -0800

    fixed letter tap, submit, and UI

--- MilestoneProject7_9_Hangman/MilestoneProject7_9_Hangman/ViewController.swift
+++ MilestoneProject7_9_Hangman/MilestoneProject7_9_Hangman/ViewController.swift
@@ -11,9 +11,18 @@ class ViewController: UIViewController {
     
     var gameArt: UILabel!
     var chosenLetter: UITextField!
-    var usedLetters: UILabel!
-    var wordToGuess: UILabel!
+    var usedLetters = [Character]()
+    var wrongAnswers: Int = 0 {
+        didSet {
+            gameArt.text = hangManArt[wrongAnswers]
+        }
+    }
+    var wordToGuess: String = ""
+    var wordToGuessLabel: UILabel!
     var letterButtons = [UIButton]()
+    var activatedButtons = [UIButton]()
+    
+    var maskedWord: String = ""
     let hangManArt = ["""
       +---+
       |   |
@@ -83,11 +92,15 @@ class ViewController: UIViewController {
         submit.translatesAutoresizingMaskIntoConstraints = false
         submit.setTitle("Submit", for: .normal)
         view.addSubview(submit)
+        submit.addTarget(self, action: #selector(submitTapped), for: .touchUpInside)
+
 
         let clear = UIButton(type: .system)
         clear.translatesAutoresizingMaskIntoConstraints = false
         clear.setTitle("Clear", for: .normal)
         view.addSubview(clear)
+        clear.addTarget(self, action: #selector(clearTapped), for: .touchUpInside)
+
         
         let buttonsView = UIView()
         buttonsView.translatesAutoresizingMaskIntoConstraints = false
@@ -101,12 +114,12 @@ class ViewController: UIViewController {
         gameArt.layer.borderWidth = 1
         view.addSubview(gameArt)
         
-        wordToGuess = UILabel()
-        wordToGuess.translatesAutoresizingMaskIntoConstraints = false
-        wordToGuess.text = ""
-        wordToGuess.font = UIFont.systemFont(ofSize: 32)
-        wordToGuess.textAlignment = .center
-        view.addSubview(wordToGuess)
+        wordToGuessLabel = UILabel()
+        wordToGuessLabel.translatesAutoresizingMaskIntoConstraints = false
+        wordToGuessLabel.text = ""
+        wordToGuessLabel.font = UIFont.systemFont(ofSize: 32)
+        wordToGuessLabel.textAlignment = .center
+        view.addSubview(wordToGuessLabel)
         
         chosenLetter = UITextField()
         chosenLetter.translatesAutoresizingMaskIntoConstraints = false
@@ -116,18 +129,17 @@ class ViewController: UIViewController {
         chosenLetter.isUserInteractionEnabled = true
         view.addSubview(chosenLetter)
         
-        
         NSLayoutConstraint.activate([
             gameArt.heightAnchor.constraint(equalToConstant: 300),
             gameArt.widthAnchor.constraint(equalToConstant: 300),
             gameArt.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             gameArt.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -200),
             
-            wordToGuess.topAnchor.constraint(equalTo: gameArt.bottomAnchor),
-            wordToGuess.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            wordToGuess.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            wordToGuessLabel.topAnchor.constraint(equalTo: gameArt.bottomAnchor),
+            wordToGuessLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            wordToGuessLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             
-            chosenLetter.topAnchor.constraint(equalTo: wordToGuess.bottomAnchor),
+            chosenLetter.topAnchor.constraint(equalTo: wordToGuessLabel.bottomAnchor),
             chosenLetter.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
             chosenLetter.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             
@@ -139,7 +151,7 @@ class ViewController: UIViewController {
             clear.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 100),
             clear.heightAnchor.constraint(equalToConstant: 35),
             
-            buttonsView.widthAnchor.constraint(equalToConstant: 200),
+            buttonsView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor),
             buttonsView.heightAnchor.constraint(equalToConstant: 200),
             buttonsView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             buttonsView.topAnchor.constraint(equalTo: submit.bottomAnchor, constant: 20),
@@ -148,8 +160,8 @@ class ViewController: UIViewController {
         ])
         
         // set some values for the width and height of each button
-        let width = 15
-        let height = 15
+        let width = 30
+        let height = 30
 
         // create 26 buttons as a 2x13 grid
         for row in 0..<2 {
@@ -170,6 +182,7 @@ class ViewController: UIViewController {
 
                 // and also to our letterButtons array
                 letterButtons.append(letterButton)
+                letterButton.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
             }
         }
         
@@ -186,27 +199,112 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view
-        beginGame()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(beginGame))
+        
+        // *******Need to fix this part
+        //beginGame(action: )
     }
     
-    func beginGame() {
-        gameArt.text = hangManArt[5]
-        wordToGuess.text = getRandomWord()
+    @objc func beginGame(action: UIAlertAction) {
+        gameArt.text = hangManArt[0]
+        wordToGuessLabel.text = getRandomWord()
 
+        for btn in activatedButtons {
+            btn.isHidden = false
+        }
+        activatedButtons.removeAll()
+        chosenLetter.text = ""
+        
+        if let letterFileURL = Bundle.main.url(forResource: "alphabet", withExtension: "txt") {
+            if let letterContents = try? String(contentsOf: letterFileURL) {
+                let letters = letterContents.components(separatedBy: "\n")
+                
+                // load letters into buttons group
+                for (index, letter) in letters.enumerated() {
+                    letterButtons[index].setTitle(letter, for: .normal)
+                }
+            }
+        }
     }
     
     func getRandomWord() -> String {
-        return maskWord(word: "text")
-        
+        wordToGuess = "rhythm"
+        return maskWord(word: wordToGuess)
     }
     
     func maskWord(word: String) -> String {
-        var maskedWord: String = ""
         for _ in word {
             maskedWord += "?"
         }
         return maskedWord
     }
+    
+    func replace(myString: String, _ index: Int, _ newChar: Character) -> String {
+        var chars = Array(myString)     // gets an array of characters
+        chars[index] = newChar
+        let modifiedString = String(chars)
+        return modifiedString
+    }
+    
+    @objc func submitTapped(_ sender: UIButton) {
+        guard let selectedLetter = chosenLetter.text else {return}
+        chosenLetter.text = ""
+        let selectedChar = Character(selectedLetter)
+        usedLetters.append(selectedChar)
+        if wordToGuess.contains(selectedLetter) {
+            print(true)
+            for (index, letter) in wordToGuess.enumerated() {
+                if String(letter) == selectedLetter {
+                    print(letter)
+                    print(selectedChar)
+                    maskedWord = replace(myString: maskedWord, index, selectedChar)
+                }
+            }
+        } else {
+            wrongAnswers += 1
+            if wrongAnswers > 5 {
+                gameOver(false)
+                return
+            }
+        }
+        wordToGuessLabel.text = maskedWord
+        if maskedWord == wordToGuess {
+            gameOver(true)
+        }
+    }
+    
+    func gameOver(_ winLose: Bool) {
+        if (winLose) {
+            let ac = UIAlertController(title: "You win!", message: nil, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Play again?", style: .default, handler: beginGame))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: "Game Over", message: "You lose!", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Play again?", style: .default, handler: beginGame))
+            present(ac, animated: true)
+        }
+    }
+    
+    @objc func clearTapped(_ sender: UIButton) {
+        chosenLetter.text = ""
+        
+        activatedButtons.last?.isHidden = false
+    }
 
-
+    
+    @objc func letterTapped(_ sender: UIButton) {
+        if chosenLetter.text != "" {
+            let ac = UIAlertController(title: "One letter already chosen!", message: "Submit or clear chosen letter.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK ", style: .default, handler: nil))
+            present(ac, animated: true)
+            return
+        }
+        guard let buttonTitle = sender.titleLabel?.text else { return }
+        chosenLetter.text = chosenLetter.text?.appending(buttonTitle)
+        activatedButtons.append(sender)
+        sender.isHidden = true
+    }
+    
+    
 }
