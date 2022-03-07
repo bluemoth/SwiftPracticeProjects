07d78f788e3f315b6cfed5c21fb98af61745c1e6
Jacob Case <jcase.bus@gmail.com>
2022-03-07 14:03:25 -0700

    project14 initial upload without challenges

---
+++ Project17_SpaceRace/Project17_SpaceRace/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project17_SpaceRace
+//
+//  Created by Jacob Case on 3/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
